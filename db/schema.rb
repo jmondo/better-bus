886ff13a40deaed2319fccd73cce68df55f830aa
John Gesimondo <john@jmondo.com>
2013-07-17 22:21:09 -0700

    no more sphinx

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20130714014326) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "stops", force: true do |t|
     t.string   "tag"
     t.string   "title"
